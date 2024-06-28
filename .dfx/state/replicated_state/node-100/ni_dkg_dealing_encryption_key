3eebd0be0abe14de041ea7339d59a971cc8e7bac
nherciu7 <nherciu7@Nichita>
2024-06-28 12:30:36 +0300

    Initial commit

---
+++ .dfx/state/replicated_state/node-100/ni_dkg_dealing_encryption_key
@@ -0,0 +1,2 @@
+	0‰nãs‰TYàµ9@û¿èÏ‡lv¯ê§!ð{œZÈÈœ'bõÇ¸e/F/Í²µx"°
+­¡xGroth20WithPop_Bls12_381£gpop_keyX0†+ôÅ¼­on}^É|XmeQÓ´ FÞç¾°™g¡—ªÄ‰P©¿Á÷„}ã6yichallengeX f}6xrø5íŒs„…àÝ‹­ïC”ÞUëêJ$;G¾4hresponseX jüLQ²—Ú¨÷Ëˆ\F'ÙCVFXÈsÒ Ù9^ñh=Í
---
+++ src/token_assets/src/components/Faucet.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import { token, canisterId, createActor } from "../../../declarations/token";
+import { AuthClient } from "@dfinity/auth-client";
+
+function Faucet() {
+
+  const [isDisabled, setDisable] = useState(false); 
+  const [buttonText, setText] = useState("Gimme gimme");
+
+  async function handleClick(event) {
+    setDisable(true);
+
+    const authClient = await AuthClient.create();
+    const identity = await authClient.getIdentity();
+
+    const authenticatedCanister = createActor(canisterId, {
+      agentOptions: {
+        identity,
+      },
+    });
+
+    const result = await authenticatedCanister.payOut();
+    setText(result);
+    
+
+  }
+
+  return (
+    <div className="blue window">
+      <h2>
+        <span role="img" aria-label="tap emoji">
+          🚰
+        </span>
+        Faucet
+      </h2>
+      <label>Get your free THEO tokens here! Claim 10,000 THEO coins to your account.</label>
+      <p className="trade-buttons">
+        <button
+         id="btn-payout" 
+         onClick={handleClick}
+         disabled={isDisabled}
+         
+         >
+          {buttonText}
+        </button>
+      </p>
+    </div>
+  );
+}
+
+export default Faucet;
